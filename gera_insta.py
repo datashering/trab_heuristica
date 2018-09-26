947a83ab955d846945c7c5508bb52590b12448dc
vtm <vinicius.t.mello.1@gmail.com>
2018-09-26 19:07:25 -0300

    Atualizado para 2 camadas

--- gera_insta.py
+++ gera_insta.py
@@ -1,45 +1,78 @@
 import numpy as np
 
-def write_instance(f, j):
+def write_instance(i, f, j, r):
+    I = range(i)
     J = range(j)
     F = range(f)
 
-    demand = np.array([int(np.random.randint(50,100)) for j in J])
-    capacity = np.array([np.random.randint(1,11) for f in F])
-    capacity = capacity * (int(demand.sum()*2/capacity.sum()))
-    f_cost = [np.random.randint(10000,50000) for f in F]
-    v_cost = [[np.random.randint(1,5) for j in J] for f in F]
+    # Gerando números aleatórios
+    p = np.random.uniform(5, 35, i)
+    h = np.random.uniform(10,160, f)
+    d = np.random.uniform(5, 35, j)
+    b = np.random.uniform(0, 90, f) + np.random.uniform(100, 110, f) * np.sqrt(h)
 
-    write_c(demand, capacity, f_cost, v_cost)
-    write_mod(demand, capacity, f_cost, v_cost)
+    # Escalando oferta com demanda
+    p = p * (d.sum() / p.sum())
+
+    # Escalando capacidade com demanda
+    h = h * (d.sum()*r / h.sum())
+
+    I_cord = np.random.uniform(0,1,(i,2))
+    F_cord = np.random.uniform(0,1,(f,2))
+    J_cord = np.random.uniform(0,1,(j,2))
+
+    dist_euc = lambda x, y: np.sqrt( (x[0] - y[0])**2 + (x[1] - y[1])**2 )
+
+    c = [[dist_euc(i,f)*10 for f in F_cord] for i in I_cord]
+    t = [[dist_euc(f,j)*10 for j in J_cord] for f in F_cord]
+
+    print(p.sum())
+    print(h.sum())
+    print(d.sum())
+
+    #write_c(p, h, d, b, c, t, r)
+    write_mod(p, h, d, b, c, t, r)
 
 
-def write_c(demand, capacity, f_cost, v_cost):
-    file = open("instancias_c/{:d}-{:d}.dat".format(len(capacity), len(demand)), 'w')
+def write_c(p, h, d, b, c, t, r):
+    file = open("instancias_c/{:d}-{:d}-{:d}-{:.1f}.dat".format(len(p), len(h), len(d), r), 'w')
 
-    file.write("{:d} {:d} ".format(len(capacity), len(demand)))
+    file.write("{:d} {:d} {:d}".format(len(p), len(h), len(d)))
     file.write("\n")
 
-    [file.write("{:d} ".format(d)) for d in demand]
+    [file.write("{:.3f} ".format(x)) for x in p]
     file.write("\n")
 
-    [file.write("{:d} ".format(b)) for b in capacity]
+    [file.write("{:.3f} ".format(x)) for x in h]
     file.write("\n")
 
-    [file.write("{:d} ".format(f)) for f in f_cost]
+    [file.write("{:.3f} ".format(x)) for x in d]
     file.write("\n")
 
-    for line in v_cost:
-        [file.write("{:d} ".format(c)) for c in line]
+    [file.write("{:.3f} ".format(x)) for x in b]
+    file.write("\n")
+
+    for line in c:
+        [file.write("{:.3f} ".format(x)) for x in line]
+        file.write("\n")
+
+    for line in t:
+        [file.write("{:.3f} ".format(x)) for x in line]
         file.write("\n")
 
     file.close()
 
-def write_mod(demand, capacity, f_cost, v_cost):
-    F = range(len(capacity))
-    J = range(len(demand))
+def write_mod(p, h, d, b, c, t, r):
 
-    file = open("instancias_mod/{:d}-{:d}.dat".format(len(F), len(J)), 'w')
+    file =  open("instancias_mod/{:d}-{:d}-{:d}-{:.1f}.dat".format(len(p), len(h), len(d), r), 'w')
+
+    I = range(len(p))
+    F = range(len(h))
+    J = range(len(d))
+
+    file.write("set I := ")
+    [file.write("{:d} ".format(i)) for i in I]
+    file.write(";\n")
 
     file.write("set F := ")
     [file.write("{:d} ".format(f)) for f in F]
@@ -49,20 +82,33 @@ def write_mod(demand, capacity, f_cost, v_cost):
     [file.write("{:d} ".format(j)) for j in J]
     file.write(";\n")
 
-    file.write("param: b q := \n")
-    [file.write("{:d}  {:d}  {:d}\n".format(f, f_cost[f], capacity[f])) for f in F]
+    file.write("param p := \n")
+    [file.write("{:d}  {:.3f}\n".format(i, p[i])) for i in I]
     file.write(";\n")
 
     file.write("param d := \n")
-    [file.write("{:d}  {:d}\n".format(j, demand[j])) for j in J]
+    [file.write("{:d}  {:.3f}\n".format(j, d[j])) for j in J]
+    file.write(";\n")
+
+    file.write("param: b h := \n")
+    [file.write("{:d}  {:.3f}  {:.3f}\n".format(f, h[f], b[f])) for f in F]
     file.write(";\n")
 
     file.write("param c : ")
-    [file.write("{:d}".format(j).ljust(5)) for j in J]
+    [file.write("{:d}".format(f).ljust(8)) for f in F]
+    file.write(":=\n")
+    for i in I:
+        file.write("{:d}".format(i).ljust(3))
+        [file.write(" {:.3f}".format(c[i][f]).ljust(8)) for f in F]
+        file.write("\n")
+    file.write(";\n")
+
+    file.write("param t : ")
+    [file.write("{:d}".format(j).ljust(8)) for j in J]
     file.write(":=\n")
     for f in F:
         file.write("{:d}".format(f).ljust(3))
-        [file.write(" {:d}".format(v_cost[f][j]).ljust(5)) for j in J]
+        [file.write(" {:.3f}".format(t[f][j]).ljust(8)) for j in J]
         file.write("\n")
     file.write(";\nend;")
 
@@ -71,13 +117,10 @@ def write_mod(demand, capacity, f_cost, v_cost):
 
 if __name__ == "__main__":
 
-    write_instance(20, 10);
-    write_instance(30, 10);
-    write_instance(20, 20);
-    write_instance(30, 20);
-    write_instance(100, 50);
-    write_instance(200,200);
-    write_instance(400,200);
-    write_instance(500,200);
-    write_instance(600,150);
-    write_instance(800,400);
+    write_instance(5, 10, 25, 1.5)
+    write_instance(5, 25, 50, 1.5)
+    write_instance(10, 50, 500, 1.5)
+    write_instance(10, 50, 500, 2.0)
+    write_instance(10, 50, 500, 3.0)
+    write_instance(10, 50, 500, 4.0)
+    write_instance(10, 100, 500, 2.0)
--- modelo_localizacao.mod
+++ modelo_localizacao.mod
@@ -1,17 +1,23 @@
-set F;  # Conjunto de Facilidades
+set I;  # Conjunto de Fabricas
+set F;  # Conjunto de Centros de districuição (CD)
 set J;  # Conjunto de Zonas de Demanda
 
-param b{f in F};          # Custo fixo de abertura de facilidade
-param q{f in F};          # Capacidade da facilidade
-param d{j in J};          # Quantidade de produto demanda por zona
-param c{f in F, j in J};  # Custo de transporte da facilidade f para a zona de demanda j
+param p{i in I};    # Capacidade de produção da fabrica
+param h{f in F};    # Capacidade do CD
+param d{j in J};    # Quantidade de produto demandado
 
+param b{f in F};          # Custo fixo de abertura do CD
+param c{i in I, f in F};  # Custo de transporte unitário de uma fabrica para um CD
+param t{f in F, j in J};  # Custo de transporte unitário de um CD para uma zona de demanda
+
+var z{i in I, j in J}, >=0;   # Quantidate transportada i -> f
 var x{f in F, j in J}, >=0;   # Quantidade transportada f -> j
 var y{f in F}, binary;        # 1 se a facilidade f é aberta, 0 se nao
 
-minimize cost: sum{f in F} y[f]*b[f] + sum{f in F, j in J} x[f,j]*c[f,j];
+minimize cost: sum{f in F} y[f]*b[f] + sum{i in I, f in F} z[i,f]*c[i,f] + sum{f in F, j in J} x[f,j]*t[f,j];
 
-r1{j in J}: sum{f in F} x[f,j] = d[j];         # Toda demanda deve ser atendida
-r2{f in F}: sum{j in J} x[f,j] <= y[f]*q[f];   # A Quantidadetrasportada deve ser menor que a capacidade
-                                               # da facilidade e zero caso a facilidade não seja aberta
+r1{j in J}: sum{f in F} x[f,j] = d[j];              # Toda demanda deve ser atendida
+r2{f in F}: sum{j in J} x[f,j] <= y[f]*h[f];        # Capacidade do CD + critério de abertura
+r3{f in F}: sum{i in I}z[i,f] = sum{j in J}x[f,j];  # Conservação de fluxo no CD
+r4{i in I}: sum{f in F}z[i,f] <= p[i];              # Capacidade de produção da fábrica
 end;
