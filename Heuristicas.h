bcd3f271bb6ff35646e0f98e7f6a17e2402f8537
vtm <vinicius.t.mello.1@gmail.com>
2018-11-06 10:25:12 -0200

    Faxina na casa

--- Heuristicas.h
+++ Heuristicas.h
@@ -6,26 +6,6 @@
 
 //  --- Heuristica Gulosa ---
 
-struct CD
-{
-  int cd;
-  double custo;
-};
-
-struct Candidatos
-{
-  //Tamanho total de candidatos
-  int tamanho;
-  //Vetor com os CDs que ainda podem ser abertos
-  std::vector<CD> viaveis;
-  //Vetor com os CDs que foram abertos
-  std::vector<int> abertos;
-  //Capacidade total dos CDs ja abertos
-  int capacidade_total;
-
-  Candidatos(int tam);
-};
-
 bool compara_custo(const CD&, const CD&);
 
 void gera_custo(Instancia&, Candidatos&);
@@ -42,23 +22,6 @@ void add(Instancia&, Solucao&, int);
 
 //  --- Heuristica Iterativa da Mochila ---
 
-struct ProgDinamica {
-  int k;
-  int demanda;
-  std::vector<double> custos;
-  std::vector<int> capacidades;
-  std::vector<bool> sol;
-
-  double **opt;
-  bool **opt_sol;
-
-  ProgDinamica(Instancia&);
-  ~ProgDinamica();
-  bool resolve();
-};
-
 void inicializa_custos(Instancia&, std::vector<double>&);
 
 void heuristica_iterativa(Instancia&, Solucao&, float);
-
-void busca_local();
--- Uteis.h
+++ Uteis.h
@@ -1,8 +1,12 @@
 #pragma once
 
-#include <iostream>
 #include <vector>
+#include <limits>
 
+//  --- Constantes e Typedef  ---
+const double MAX = std::numeric_limits<double>::max();
+
+// Struct para guardar instancia
 struct Instancia
 {
   int I;                      //Numero de fabricas
@@ -16,10 +20,11 @@ struct Instancia
   double **t;                    //Custo de transporter unitario de um CD para um ponto de demanda
   int d_total;
 
-  Instancia(const char *nome);
+  Instancia(const char*);
   ~Instancia();
 };
 
+// Struct para guardar solução
 struct Solucao
 {
   int I, F, J;
@@ -32,3 +37,44 @@ struct Solucao
   Solucao(int, int, int);
   ~Solucao();
 };
+
+struct CD
+{
+  int cd;
+  double custo;
+};
+
+struct Candidatos
+{
+  //Tamanho total de candidatos
+  int tamanho;
+  //Vetor com os CDs que ainda podem ser abertos
+  std::vector<CD> viaveis;
+  //Vetor com os CDs que foram abertos
+  std::vector<int> abertos;
+  //Capacidade total dos CDs ja abertos
+  int capacidade_total;
+
+  Candidatos(int tam);
+};
+
+// Struct para resolver problema com programação dinamica
+struct ProgDinamica {
+  int k;
+  int demanda;
+  std::vector<double> custos;
+  std::vector<int> capacidades;
+  std::vector<bool> sol;
+
+  double **opt;
+  bool **opt_sol;
+
+  ProgDinamica(Instancia&);
+  ~ProgDinamica();
+  bool resolve();
+};
+
+// Funcoes diversas
+double abre_aprox(Instancia&, const Solucao&, int);
+double fecha_aprox(Instancia&, const Solucao&, int);
+double avalia_t1b(Instancia&, Solucao&, int);
